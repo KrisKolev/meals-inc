9188e27aae373a8160c3daa5a1db8af3fcde0ad3
i455802 <k.kolev@student.fontys.nl>
2021-11-05 19:30:30 +0100

    Made Research document, did frontend work, also ci test

--- RestaurantApplicationBackend/src/main/java/com/example/restaurantapplication/repository/MockDataRestaurant.java
+++ RestaurantApplicationBackend/src/main/java/com/example/restaurantapplication/repository/MockDataRestaurant.java
@@ -30,13 +30,7 @@ public class MockDataRestaurant
         this.tablesList = new ArrayList<>(List.of(
                 new Table(0),
                 new Table(1),
-                new Table(2),
-                new Table(3),
-                new Table(4),
-                new Table(5),
-                new Table(6),
-                new Table(7),
-                new Table(8)
+                new Table(2)
         ));
     }
 
